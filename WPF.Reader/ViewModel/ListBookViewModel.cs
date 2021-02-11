2802ab445ebfcb67b8b09f9e25816b564d4e5c1c
tc06580 <cyril.tisserand@docapost.fr>
2021-02-11 08:41:28 +0100

    Add Template for WPF

---
+++ WPF.Reader/ViewModel/ListBookViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF.Reader.Model;
+
+namespace WPF.Reader.ViewModel
+{
+    class ListBookViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand ItemSelectedCommand { get; set; }
+
+        // To replace with your own data !!!!!!!!!!!!!!
+        public ObservableCollection<Book> Books { get; set; } = new ObservableCollection<Book>() { new Book() };
+
+        public ListBookViewModel()
+        {
+            ItemSelectedCommand = new RelayCommand(obj =>
+            {
+                var item = obj as Book;
+                Navigator.Instance.CurrentPage = PageEnum.DETAIL;
+            });
+        }
+    }
+}
